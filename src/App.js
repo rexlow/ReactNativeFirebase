501b5ad02d94d3b6230a00fd9719be38d75a39e8
Rex Low <qiweilow950823@gmail.com>
2016-10-14 16:54:09 +0800

    Final wrap up

--- src/App.js
+++ src/App.js
@@ -11,22 +11,53 @@ import Button from './components/Button';
 import Card from './components/Card';
 import CardSection from './components/CardSection';
 import LoginForm from './components/LoginForm';
+import Spinner from './components/Spinner';
 
 class App extends Component {
+  state = {loggedIn: null}
+
   componentWillMount() {
     firebase.initializeApp({
+      //define firebase config here
       apiKey: "AIzaSyDLthtViPsRw2je3PByFMnL1-yfeuJy3kE",
       authDomain: "authentication-86718.firebaseapp.com",
       databaseURL: "https://authentication-86718.firebaseio.com",
       storageBucket: "authentication-86718.appspot.com",
       messagingSenderId: "691068630838"
     });
+
+    //handle user authentication activity
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user) {
+        this.setState({loggedIn: true});
+      } else {
+        this.setState({loggedIn: false});
+      }
+    });
   }
+
+  renderContent() {
+    switch(this.state.loggedIn){
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Sign Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
   render() {
     return(
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        <View>
+          {this.renderContent()}
+        </View>
       </View>
     )
   }
